dab9edd4f30da6451bf1d4ea9f1aa9fcf3f03b34
Chandler Childs <chandler@advenna.com>
2021-03-01 10:41:42 -0700

    add api test

--- test/pop_web/controllers/temperature_controller_test.exs
+++ test/pop_web/controllers/temperature_controller_test.exs
@@ -8,7 +8,7 @@ defmodule PopWeb.Controllers.TemperatureControllerTest do
         |> get("/api/temperature")
         |> json_response(200)
 
-      assert resp == %{"temps" => []}
+      assert resp == %{"temps" => [-459.67, 32, 212, 0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]}
     end
   end
 end
