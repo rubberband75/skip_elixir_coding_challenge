48f6ecf210580416853043e5a3f9d4af90c82b39
Chandler Childs <chandler@advenna.com>
2021-03-01 11:16:09 -0700

    add api tests

--- test/pop_web/controllers/temperature_controller_test.exs
+++ test/pop_web/controllers/temperature_controller_test.exs
@@ -10,5 +10,23 @@ defmodule PopWeb.Controllers.TemperatureControllerTest do
 
       assert resp == %{"temps" => [-459.67, 32, 212, 0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]}
     end
+
+    test "temps array not empty", %{conn: conn} do
+      resp =
+        conn
+        |> get("/api/temperature")
+        |> json_response(200)
+
+      assert (length resp["temps"]) >  0
+    end
+
+    test "all temps are numbers", %{conn: conn} do
+      resp =
+        conn
+        |> get("/api/temperature")
+        |> json_response(200)
+
+      assert Enum.all?(resp["temps"], fn x -> is_number(x) end)
+    end
   end
 end
